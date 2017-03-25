85417bb35ee0988363c38135e23d0486732b3bde
vthb1992 <hahavthb@gmail.com>
2017-03-25 17:57:25 +0800

    initial commit - added othellox.c

--- README.md
+++
@@ -1,1 +0,0 @@
-# othelloxProject2
---
+++ othellox/othellox/othellox.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+void readFile(char* initialbrd, char* evalparams) {
+
+	printf("hello");
+}
+
+int main(int argc, char **argv)
+{
+	readFile(argv[1], argv[2]);
+
+	getch();
+	return 0;
+}
