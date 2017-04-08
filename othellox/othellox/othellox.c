ef5b5d6d078e0b0359ad54d2efd3387a66590ac8
vthb1992 <hahavthb@gmail.com>
2017-04-08 17:15:45 +0800

    Added findAllLegalMoves function

--- othellox/othellox/othellox.c
+++ othellox/othellox/othellox.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <time.h>
 #include <string.h>
 
 #define EMPTY 0
 #define BLACK 1
 #define WHITE 2
 
+typedef enum { false, true } bool;
+
+const int DIRECTION[8][2] = { { 1, 0 },{ 1, 1 },{ 0, 1 },{ -1, 1 },{ -1, 0 },{ -1, -1 },{ 0, -1 },{ 1, -1 } };
+
 char board[676];
+char lmBoard[676];
 
 int size_x, size_y;
-char white_positions[400][5];
-char black_positions[400][5];
+char white_positions[350][5];
+char black_positions[350][5];
 int black_size, white_size;
 
 int maxDepth, maxBoards, cornerValue, edgeValue;
@@ -149,21 +156,103 @@ char *translateIndexToOutputPos(int index) {
 void initBoard() {
 	for (int a = 0; a < size_x * size_y; a++) {
 		board[a] = EMPTY;
+		
+		lmBoard[a] = EMPTY;
 	}
 	for (int i = 0; i < white_size; i++) {
 		board[translateInputPosToIndex(white_positions[i])] = WHITE;
+		
+		lmBoard[translateInputPosToIndex(white_positions[i])] = WHITE;
 
 	}
 	for (int j = 0; j < black_size; j++) {
 		board[translateInputPosToIndex(black_positions[j])] = BLACK;
+		
+		lmBoard[translateInputPosToIndex(black_positions[j])] = BLACK;
+	}
+}
+
+bool isLegalMove(char *board, int index, int legalMoveFor) { //legal move for which player color white/black
+	int x = index + 1;
+	int y = 1;
+	while (x > size_x) {
+		x = x - size_x;
+		y++;
 	}
 
+	if (board[index] != EMPTY) {
+		return false;
+	}
+	
+	for (int dir = 0; dir < 8; dir++) { // checking for legal moves in all 8 directions, horizontal, vertial and diagonal
+		int dx = DIRECTION[dir][0];
+		int dy = DIRECTION[dir][1];
+		int tempx = x;
+		int tempy = y;
+		int numFlipped = 0;
+
+		bool cont = true;
+		while (cont) {
+			tempx = tempx + dx;
+			tempy = tempy + dy;
+
+			if (tempx <= 0 || tempx > size_x || tempy <= 0 || tempy > size_y) {
+				cont = false;
+				continue;
+			}
+
+			int tempIndex = (tempx - 1) + (tempy - 1) * size_x;
+			if (board[tempIndex] == EMPTY) {
+				cont = false;
+			}
+			else if(board[tempIndex] == WHITE){
+				if (legalMoveFor == BLACK) {
+					numFlipped++;
+				}
+				else {
+					if (numFlipped == 0) {
+						cont = false;
+					}
+					else {
+						return true;
+					}
+				}
+			}
+			else {
+				if (legalMoveFor == WHITE) {
+					numFlipped++;
+				}
+				else {
+					if (numFlipped == 0) {
+						cont = false;
+					}
+					else {
+						return true;
+					}
+				}
+			}
+		}
+	}
+	return false;
 }
+
+bool findAllLegalMoves(char *board, int legalMoveFor, char *lmBoard) {
+	bool result = false;
+	int sizeOfArray = size_x * size_y;
+	for (int i = 0; i < sizeOfArray; i++) {
+		if (isLegalMove(board, i, legalMoveFor)) {
+				result = true;
+				lmBoard[i] = 3;
+		}
+	}
+	return result;
+}
+
 void printBoard() {
 	// A-Z on the x-axis(left to right), 1-26 on the y-axis(top to bottom)
 	int sizeOfArray = size_x * size_y;
 	for (int a = 0; a < sizeOfArray; a++) {
-		printf("%d", board[a]);
+		printf("%d", lmBoard[a]);
 		if ((a+1) % size_x == 0) {
 			printf("\n");
 		}
@@ -174,9 +263,9 @@ int main(int argc, char **argv)
 	readFiles(argv[1], argv[2]);
 
 	initBoard();
-	printBoard();
 	
-
+	findAllLegalMoves(board, 2, lmBoard);
+	printBoard();
 
 	//printf("%d", maxBoards);
 	getch();
